654044f8510db97051c7c4b37def8b4b345540f7
Hamoir Chloe <chloehamoir@gmail.com>
2024-11-25 11:35:42 +0100

    genre API

--- movies.js
+++ movies.js
@@ -12,6 +12,10 @@ const searchInput = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
 const searchContainer = document.getElementById('search');
 const latestContainer = document.getElementById('latest');
+const genreList = document.querySelectorAll('.movies-by-genre ul li');
+const displayGenreContainer = document.querySelector('.display-genre');
+
+
 
 // Fonction pour récupérer les films depuis l'API en fonction de la recherche
     async function fetchMovies(query) {
@@ -80,10 +84,49 @@ async function displayUpcomingInSwiper() {
 // Appel de displayUpcomingInSwiper au chargement de la page pour afficher les derniers films à venir
 displayUpcomingInSwiper();
 
-// un display selon un genre 
-async function genreSearch() {
+
+
+
+const genreMap = {
+    0: 35, // Comedy
+    1: 18, // Drama
+    2: 28, // Action
+    3: 10749, // Romance
+    4: 14, // Fantasy
+    5: 16, // Animation
+};
+
+
+async function genreSearch(genreId) {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}`, options);
-    const data = await response.json(); // Convertir en JSON pour récupérer les résultats
+    const data = await response.json();
     return data.results;
-} 
+}
+
+function displayMoviesGenre(movies) {
+    if (movies.length === 0) {
+        genresContainer.innerHTML = `<p>Aucun film trouvé pour ce genre.</p>`;
+        return;
+    }
+
+    displayGenreContainer.innerHTML = `
+    <swiper-container class="image-container" navigation="true" space-between="20" slides-per-view="4" mousewheel="true">
+        ${movies.map(movie => `
+            <swiper-slide>
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            </swiper-slide>
+        `).join('')}
+    </swiper-container>
+`;
+}
+
+genreList.forEach((li, index) => {
+    li.addEventListener('click', async () => {
+        const genreId = genreMap[index]; // Obtenir l'ID du genre depuis la carte
+        if (genreId) {
+            const movies = await genreSearch(genreId); // Rechercher les films pour ce genre
+            displayMoviesGenre(movies); // Afficher les films
+        }
+    });
+});
 
